27794906333ee4c473ee6b214d7a814923d6f072
smith <navergirugi@gmail.com>
2025-08-14 12:01:27 +0900

    tailwind 설정 수정 및 github 릴리즈 자동으로 안되게 수정 오류 수정

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,10 +1,10 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   // Tailwind가 스타일을 적용할 파일들의 경로를 지정합니다.
-  // UI 코드가 있는 'renderer' 폴더를 가리키도록 설정합니다.
+  // renderer 폴더에서 실행되므로 현재 폴더 기준으로 설정합니다.
   content: [
-    './renderer/pages/**/*.{js,ts,jsx,tsx}',
-    './renderer/components/**/*.{js,ts,jsx,tsx}', // components 폴더도 포함
+    './pages/**/*.{js,ts,jsx,tsx}',
+    './components/**/*.{js,ts,jsx,tsx}', // components 폴더도 포함
   ],
   theme: {
     extend: {},
